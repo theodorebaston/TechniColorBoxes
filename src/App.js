baf7a6fce2f7dcb4ae2d16197390e100e414641d
theodorebaston <theodorebaston@gmail.com>
2024-12-12 15:23:50 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box-box">
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+        <div className="color-box"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

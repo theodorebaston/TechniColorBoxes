f8c6dac65f2745c36bd4d8caebe26ce1d7b7814c
theodorebaston <theodorebaston@gmail.com>
2024-12-20 18:02:18 -0500

    Followed React tutorial and set up App.js for future success

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import './App.css';
 
 function Box({color}){
   return (
-    <div className="color-box" style={{backgroundColor: color}}></div>
+    <div className="colorBox" style={{backgroundColor: color}}></div>
   )
 }
+
+function BoxBox({numberOfBoxes}) {
+  numberOfBoxes = 9
+  return (
+    <>
+    <div className="boxBox">
+
+    {/* Next Goal: Figure out how to deploy Boxes via variable numberOfBoxes, rather than hard coding them. Obv it's a for loop. Just need to figure out the proper syntax within React */}
+
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+      <Box color="#666b75" />
+    </div>
+    </>
+  )
+}
+
 
 export default function App() {
   return (
     <>
     <div className="App">
-      <div className="box-box">
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-        <Box color="#666b75" />
-      </div>
+      <BoxBox numberOfBoxes="9"/>
     </div>
     </>
 

e6fe8dfc998c3a4c750208ea35383152a16fe455
theodorebaston <theodorebaston@gmail.com>
2025-01-02 12:30:00 -0500

    The boxes change color!

--- src/box.js
+++ src/box.js
@@ -1,5 +1,18 @@
-export default function Box({color}){
-    return (
-      <div className="colorBox" style={{backgroundColor: color}}></div>
-    )
-  }
+'use client';
+
+import { useState } from 'react';
+
+export default function Box(){
+
+  const [color, setColor] = useState('666b75');
+
+  const handleColorChange = () => {
+      setColor(color === '666b75' ? '#78568e' : '666b75');
+  } 
+  
+  return (
+    <>
+    <div onMouseEnter={handleColorChange} className="colorBox" style={{backgroundColor: color}}></div>
+    </>
+  )
+}
